47a6163c729bbaf46d660c72de0527f56f07ce94
stineb <benjamin.stocker@gmail.com>
2016-11-29 17:36:23 +0100

    moved data files (csv) to subdirectory.

--- example_plot.R
+++ example_plot.R
@@ -3,10 +3,10 @@
 ##--------------------------------------------------------------------------
 rm( list=ls() )
 
-peat_yml_lhnfix     <- read.csv( file="dcpeat_yml_stocker17pnas.csv", skip=30 )
-peat_yml_lhnfix_cum <- read.csv( file="dcpeat_cum_yml_stocker17pnas.csv", skip=33 )
-peat_lpx            <- read.csv( file="dcpeat_lpx_stocker17pnas.csv", skip=26 )
-fluc                <- read.csv( file="luc_co2_emissions_stocker17pnas.csv", skip=30 )
+peat_yml_lhnfix     <- read.csv( file="data/dcpeat_yml_stocker17pnas.csv", skip=30 )
+peat_yml_lhnfix_cum <- read.csv( file="data/dcpeat_cum_yml_stocker17pnas.csv", skip=33 )
+peat_lpx            <- read.csv( file="data/dcpeat_lpx_stocker17pnas.csv", skip=26 )
+fluc                <- read.csv( file="data/luc_co2_emissions_stocker17pnas.csv", skip=30 )
 
 add_alpha <- function( col, alpha ){
   ## add alpha to color given as a name
@@ -20,75 +20,72 @@ add_alpha <- function( col, alpha ){
 off            <- 24.654
 off_lpx        <- 285.1803
 
-    print("plotting ... ")
-    ##--------------------------------
-    ## Annual eat C balance
-    ##--------------------------------
-    xlim <- c(-10000,2000)
-    plot( range(peat_yml_lhnfix$year), range(peat_yml_lhnfix$mean), type="n", ylab=expression( "annual peat C balance (PgC" ~ yr^{-1} ~ ")" ), xlab="", xlim=xlim, ylim=c(-0.01,0.12) )
-    
-    ## add +/- 1-sigma range to plot
-    lines( peat_yml_lhnfix$year, (peat_yml_lhnfix$mean - peat_yml_lhnfix$sd) )
-    lines( peat_yml_lhnfix$year, (peat_yml_lhnfix$mean + peat_yml_lhnfix$sd) )
+print("plotting ... ")
+##--------------------------------
+## Annual eat C balance
+##--------------------------------
+xlim <- c(-10000,2000)
+plot( range(peat_yml_lhnfix$year), range(peat_yml_lhnfix$mean), type="n", ylab=expression( "annual peat C balance (PgC" ~ yr^{-1} ~ ")" ), xlab="", xlim=xlim, ylim=c(-0.01,0.12) )
 
-    ## PEAT Add mean to plot
-    lines( peat_yml_lhnfix$year, peat_yml_lhnfix$mean, col="dodgerblue4", lwd=2, lty=1 )
+## add +/- 1-sigma range to plot
+lines( peat_yml_lhnfix$year, (peat_yml_lhnfix$mean - peat_yml_lhnfix$sd) )
+lines( peat_yml_lhnfix$year, (peat_yml_lhnfix$mean + peat_yml_lhnfix$sd) )
 
-    ## Add model NEP to plot
-    boxl <- 1
-    lines( peat_lpx$year, peat_lpx$nep, lwd=1, col="dodgerblue2" )
-    boxl <- 30
-    lines( peat_lpx$year, peat_lpx$nep, lwd=2, col="dodgerblue2" )
+## PEAT Add mean to plot
+lines( peat_yml_lhnfix$year, peat_yml_lhnfix$mean, col="dodgerblue4", lwd=2, lty=1 )
 
-    ##--------------------------------
-    ## Cumulative eat C balance
-    ##--------------------------------
-      par(las=1, xaxs="i")
-      ylim <- c(0,650)
-      plot( range(peat_yml_lhnfix$year), ylim, type="n", ylab="cumulative C uptake (PgC)", xlab="year CE", xlim=xlim, ylim=ylim )
+## Add model NEP to plot
+boxl <- 1
+lines( peat_lpx$year, peat_lpx$nep, lwd=1, col="dodgerblue2" )
+boxl <- 30
+lines( peat_lpx$year, peat_lpx$nep, lwd=2, col="dodgerblue2" )
 
-      ## add +/- 1-sigma range to plot
-      par( new=TRUE )
-      plot( range(peat_yml_lhnfix$year), ylim, type="n", ylab="", xlab="", xlim=xlim, ylim=ylim, axes=FALSE )
-      polygon( 
-        c( peat_yml_lhnfix$year, rev(peat_yml_lhnfix$year) ), 
-        c( (peat_yml_lhnfix_cum$mean - peat_yml_lhnfix_cum$sd) - off, rev((peat_yml_lhnfix_cum$mean + peat_yml_lhnfix_cum$sd) - off) ),
-        col=add_alpha( "dodgerblue4", 0.3 ), border=NA 
-      )
+##--------------------------------
+## Cumulative eat C balance
+##--------------------------------
+  par(las=1, xaxs="i")
+  ylim <- c(0,650)
+  plot( range(peat_yml_lhnfix$year), ylim, type="n", ylab="cumulative C uptake (PgC)", xlab="year CE", xlim=xlim, ylim=ylim )
 
-      # ## Add mean to plot
-      lines( peat_yml_lhnfix_cum$year, peat_yml_lhnfix_cum$mean - off, col="dodgerblue4", lwd=2, lty=1 )
+  ## add +/- 1-sigma range to plot
+  par( new=TRUE )
+  plot( range(peat_yml_lhnfix$year), ylim, type="n", ylab="", xlab="", xlim=xlim, ylim=ylim, axes=FALSE )
+  polygon( 
+    c( peat_yml_lhnfix$year, rev(peat_yml_lhnfix$year) ), 
+    c( (peat_yml_lhnfix_cum$mean - peat_yml_lhnfix_cum$sd) - off, rev((peat_yml_lhnfix_cum$mean + peat_yml_lhnfix_cum$sd) - off) ),
+    col=add_alpha( "dodgerblue4", 0.3 ), border=NA 
+  )
 
-      # ## Add model total peat C
-      lines( peat_lpx$year, peat_lpx$totc - off_lpx, lwd=1, col="dodgerblue2" )
-      lines( smooth.spline( peat_lpx$year, peat_lpx$totc - off_lpx, spar=0.7 ), lwd=2, col="dodgerblue2")
+  # ## Add mean to plot
+  lines( peat_yml_lhnfix_cum$year, peat_yml_lhnfix_cum$mean - off, col="dodgerblue4", lwd=2, lty=1 )
 
-      # off <- cumsum( peat_lpx$nep )[which( peat_lpx$year==-8995 )] * 10
-      # lines( peat_lpx$year, cumsum( peat_lpx$nep ) * 10 - off, lty=2, lwd=2, col="dodgerblue2" )
+  # ## Add model total peat C
+  lines( peat_lpx$year, peat_lpx$totc - off_lpx, lwd=1, col="dodgerblue2" )
+  lines( smooth.spline( peat_lpx$year, peat_lpx$totc - off_lpx, spar=0.7 ), lwd=2, col="dodgerblue2")
 
-      legend( "topleft", c( 
-        expression(paste("peat, YML (",Delta ,"C"[peat],")")), 
-        expression(paste("peat, LPX (",Delta ,"C"[peat],")"))
-        ), lty=1, lwd=2, col=c("dodgerblue4","dodgerblue2"), bty="n" )
+  legend( "topleft", c( 
+    expression(paste("peat, YML (",Delta ,"C"[peat],")")), 
+    expression(paste("peat, LPX (",Delta ,"C"[peat],")"))
+    ), lty=1, lwd=2, col=c("dodgerblue4","dodgerblue2"), bty="n" )
 
-    ##--------------------------------
-    ## HOLOCENE
-    ##--------------------------------
-      ylim <- c(-400,100)
-      plot( range(fluc$year), ylim, type="n", ylab="cumulative C uptake (PgC)", xlab="ka BP", xlim=xlim, ylim=ylim )
+##--------------------------------
+## HOLOCENE
+##--------------------------------
+  ylim <- c(-400,100)
+  plot( range(fluc$year), ylim, type="n", ylab="cumulative C uptake (PgC)", xlab="ka BP", xlim=xlim, ylim=ylim )
 
-      lines( fluc$year, -fluc$hyde31_cum, col="tomato", lwd=1.5  )
-      polygon( c(fluc$year, rev(fluc$year)), c(-fluc$hyde31_cum, rev(-fluc$hyde31u_cum)), col="tomato", border=NA )
+  lines( fluc$year, -fluc$hyde31_cum, col="tomato", lwd=1.5  )
+  polygon( c(fluc$year, rev(fluc$year)), c(-fluc$hyde31_cum, rev(-fluc$hyde31u_cum)), col="tomato", border=NA )
 
-      lines( fluc$year, -fluc$hyde32_cum, col="orchid", lwd=1.5  )
-      polygon( c(fluc$year, rev(fluc$year)), c(-fluc$hyde32_cum, rev(-fluc$hyde32u_cum)), col="orchid", border=NA )
+  lines( fluc$year, -fluc$hyde32_cum, col="orchid", lwd=1.5  )
+  polygon( c(fluc$year, rev(fluc$year)), c(-fluc$hyde32_cum, rev(-fluc$hyde32u_cum)), col="orchid", border=NA )
 
-      lines( fluc$year, -fluc$kk10_cum, col="turquoise3", lwd=1.5, lty=2 )
-      lines( fluc$year, -fluc$kk10d_cum, col="turquoise3", lwd=1.5 )
+  lines( fluc$year, -fluc$kk10_cum, col="turquoise3", lwd=1.5, lty=2 )
+  lines( fluc$year, -fluc$kk10d_cum, col="turquoise3", lwd=1.5 )
 
-      lines( fluc$year, -fluc$hyde31_cum )
-      lines( fluc$year, -fluc$hyde32_cum )
-      lines( fluc$year, -fluc$kk10_cum)
-      lines( fluc$year, -fluc$kk10d_cum )
+  lines( fluc$year, -fluc$hyde31_cum )
+  lines( fluc$year, -fluc$hyde32_cum )
+  lines( fluc$year, -fluc$kk10_cum)
+  lines( fluc$year, -fluc$kk10d_cum )
 
-      lines( fluc$year, -cumsum(fluc$kk10d) )
+  lines( fluc$year, -cumsum(fluc$kk10d) )
